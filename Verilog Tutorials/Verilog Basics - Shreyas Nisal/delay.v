17c0edd3f995ff0bf2ef9ef475831e3726d9687a
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2024-04-13 08:27:41 +0600

    delay

---
+++ Verilog Tutorials/Verilog Basics - Shreyas Nisal/delay.v
@@ -0,0 +1,15 @@
+module testbench;
+
+    initial 
+    begin
+        #5 //while both initial blocks start at the same time, this delay will ensure that the commands after it runs after 5 time units
+        $display("Hello");
+    end
+
+    initial
+    begin
+        $display("World!");
+    end 
+
+
+endmodule
