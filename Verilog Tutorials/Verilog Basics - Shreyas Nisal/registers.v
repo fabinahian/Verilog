3d71a2a5e66338c3c7ae042cad70cce73ec81998
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2024-04-13 08:35:14 +0600

    registers

---
+++ Verilog Tutorials/Verilog Basics - Shreyas Nisal/registers.v
@@ -0,0 +1,18 @@
+module testbench;
+
+    reg a; //registers are variables; they can store values
+
+    initial 
+    begin
+        a = 0; //giving 'a' an initial value
+        $display("Start!");
+        #10 $finish;
+    end
+
+    always
+    begin
+        #1
+        a = !a;
+        $display(a);
+    end
+endmodule
