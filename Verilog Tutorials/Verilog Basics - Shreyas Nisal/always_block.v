3492118b89f6de91ec3d748944c2cd8abe6cfcd2
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2024-04-13 08:31:36 +0600

    always block

---
+++ Verilog Tutorials/Verilog Basics - Shreyas Nisal/always_block.v
@@ -0,0 +1,15 @@
+module testbench;
+
+    initial
+    begin
+        $display("Start!");
+        #10 $finish; //you need to end the program; otherwise the always block will go into forever loop
+    end
+
+    always
+    begin
+        #5 //you need a delay for the always block to loop
+        $display("always");
+    end
+
+endmodule
