500f65aec2becad6dd121a611b5012a67f66fae0
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2024-04-16 03:14:04 +0600

    register width

--- Verilog Tutorials/Verilog Basics - Shreyas Nisal/08_register_width.v
+++ Verilog Tutorials/Verilog Basics - Shreyas Nisal/08_register_width.v
@@ -2,6 +2,8 @@ module testbench;
     reg a; //one bit register
     reg [2:0] b; //three bit register
     reg [2:0] c;
+    reg [2:0] d;
+    reg e;
 
     initial begin
         a = 4; // 4 in binary = 100
@@ -18,6 +20,14 @@ module testbench;
         c[0] = 1'b0; c[1] = 1'b0;
 
         $display(c); //will  get a 'X' because we didn't assign value to every position
+
+        {e,d} = 4'd10; 
+        /*
+        since 10 requires 4 bits and d is a 3-bit regiters, 3 bits from the right will get assigned to 'd' and the left-out bit will get assigned to 3. 
+        So, 
+        10 = binary 1010
+        e = 1, d = 010
+        */
         $finish;
     end
 endmodule
