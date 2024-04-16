e0bb4eb5bc1f4950aef1c72eb0466f9967aff86d
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2024-04-16 17:49:38 +0600

    updated structural half adder

--- Verilog Tutorials/Verilog Tutorials for Beginners - Knowledge Unlimited/Half Adder/structural.v
+++ Verilog Tutorials/Verilog Tutorials for Beginners - Knowledge Unlimited/Half Adder/structural.v
@@ -8,7 +8,41 @@ module half_Adder (
   input a, b;
   output carry, sum;
 
-  
+  xor (sum, a, b);
+  and (carry, a, b);
 
+endmodule
+
+`timescale 1ps / 1ps
+
+module testbench;
+
+  reg a, b;
+  wire carry, sum;
+
+  initial begin
+    a = 1'b0;
+    b = 1'b0;
+
+    #2 a = 1'b0;
+    b = 1'b1;
+
+    #2 a = 1'b1;
+    b = 1'b0;
+
+    #2 a = 1'b1;
+    b = 1'b1;
+
+    #2 $finish;
+  end
+
+  initial $monitor("time = %g, a = %b, b = %b, carry = %b, sum = %b", $time, a, b, carry, sum);
+
+  half_Adder ha1 (
+      a,
+      b,
+      carry,
+      sum
+  );
 
 endmodule
