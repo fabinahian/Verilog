280ac16700e6774faccea0f2d63fa5f9ac662968
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2024-04-16 18:08:06 +0600

    DATA FLOW

---
+++ Verilog Tutorials/Verilog Tutorials for Beginners - Knowledge Unlimited/Half Adder/data_flow.v
@@ -0,0 +1,58 @@
+module half_adder (
+    a,
+    b,
+    carry,
+    sum
+);
+
+  input a, b;
+  output carry, sum;
+
+  assign carry = a & b;  // & = and
+  assign sum   = a ^ b;  // ^ = XOR
+
+endmodule
+
+`timescale 1ns / 1ps
+
+module testbench;
+
+  reg a, b;
+  wire carry, sum;
+
+
+  initial begin
+    a = 1'b0;
+    b = 1'b0;
+
+    #2 a = 1'b0;
+    b = 1'b1;
+
+    #2 a = 1'b1;
+    b = 1'b0;
+
+    #2 a = 1'b1;
+    b = 1'b1;
+
+  end
+
+
+
+  initial $monitor("time = %g, a = %b, b = %b, carry = %b, sum = %b", $time, a, b, carry, sum);
+
+  half_adder dut (
+      a,
+      b,
+      carry,
+      sum
+  );
+
+  initial #10 $finish;
+endmodule
+
+/*OUTPUT:
+time = 0, a = 0, b = 0, carry = 0, sum = 0
+time = 2, a = 0, b = 1, carry = 0, sum = 1
+time = 4, a = 1, b = 0, carry = 0, sum = 1
+time = 6, a = 1, b = 1, carry = 1, sum = 0
+*/
